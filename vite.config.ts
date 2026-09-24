f6ba43680480cfefe24d05a347cedc34859f3868
Shivang <dshivang2002@gmail.com>
2026-09-25 04:21:47 +0530

    asssss

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,12 @@ export default defineConfig({
 
   nitro: {
     preset: "vercel",
+
+    externals: {
+      inline: [
+        "tslib",
+        "@supabase/functions-js",
+      ],
+    },
   },
 });
